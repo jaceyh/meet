2a8d59e79ccaf7c5cfa3f746d88b64fb341002f4
jaceyh <jacey.hallock@gmail.com>
2023-11-21 10:32:20 -0500

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import mockData from "./mock-data";
 
 /**
- *
  * @param {*} events:
  * The following function should be in the “api.js” file.
  * This function takes an events array, then uses map to create a new array with only locations.
@@ -15,10 +14,56 @@ export const extractLocations = (events) => {
     return locations;
 };
 
-/**
- *
- * This function will fetch the list of all events
- */
- export const getEvents = async () => {
-    return mockData;
+const checkToken = async (accessToken) => {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+    const result = await response.json();
+    return result;
   };
+  
+
+/** This function will fetch the list of all events */
+ export const getEvents = async () => {
+    if (window.location.href.startsWith('http://localhost')) {
+        return mockData;
+    }
+    
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url =  "YOUR_GET_EVENTS_API_ENDPOINT" + "/" + token;
+        const response = await fetch(url);
+        const result = await response.json();
+        if (result) {
+          return result.events;
+        } else return null; 
+      }
+  };
+
+/**This function will get the access token */
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        
+        if (!code) {
+        const response = await fetch(
+        "https://w99kauf38h.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const result = await response.json();
+        const { authUrl } = result;
+        return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+
+
+};
