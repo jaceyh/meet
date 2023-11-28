572f3fb62dc9b960a9ae777a4b9ad1a4ee11d0a7
jaceyh <jacey.hallock@gmail.com>
2023-11-28 17:23:32 -0500

    install jest-cucumber, remove jest-preview

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,6 @@
 import { render, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { getEvents } from '../api';
-import { debug } from 'jest-preview';
 import App from '../App';
 
 describe('<App /> component', () => {
@@ -12,17 +11,14 @@ describe('<App /> component', () => {
 
     test('renders list of events', () => {
         expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
-        debug();
     });
     
     test('render CitySearch', () => {
         expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-        debug();
     });
 
     test('render NumberOfEventsComponent', () => {
         expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
-        debug();
     });
          
 });
