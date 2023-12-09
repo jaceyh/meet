bae607691dd7e1ab041f486d7465bad7b5ed9188
jaceyh <jacey.hallock@gmail.com>
2023-12-09 13:26:13 -0500

    first test in specifyNumber passing, work on test 2 next

--- src/features/specifyNumberOfEvents.feature
+++ src/features/specifyNumberOfEvents.feature
@@ -1,10 +1,10 @@
 Feature: Specify Number of Events
-    Scenario: When user hasn’t specified a number, 32 events are shown by default.
+    Scenario: When user hasnt specified a number, 32 events are shown by default.
         Given  user hasnt selected a specific number of events to be shown
-        When  user selects their city
+        When  user sees a list of all events
         Then the default amount of events, 32, are shown
 
     Scenario: User can change the number of events displayed.
-        Given user would like to view a smaller or larger number of events
+        Given user sees 32 events and would like to view another number of events
         When user inputs another number 
         Then user will be shown the amount of events they have input
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -9,22 +9,30 @@ const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
 
-    test('When user hasn’t specified a number, 32 events are shown by default.', ({ given, when, then }) => {
+    test('When user hasnt specified a number, 32 events are shown by default.', ({ given, when, then }) => {
+
+        let AppComponent;
         given('user hasnt selected a specific number of events to be shown', () => {
-
+            AppComponent = render(<App />);
         });
 
-        when('user selects their city', () => {
-
+        when('user sees a list of all events', async () => {
+            const AppDOM = AppComponent.container.firstChild;
+            await waitFor(() => {
+                const eventList = within(AppDOM).queryAllByRole('listitem');
+                expect(eventList[0]).toBeInTheDocument();
+            })
         });
 
-        then('the default amount of events, 32, are shown', (arg0) => {
-
+        then('the default amount of events, 32, are shown', () => {
+            const AppDOM = AppComponent.container.firstChild;
+            const eventList = within(AppDOM).queryAllByRole('listitem');
+            expect(eventList.length).toEqual(32);
         });
     });
 
     test('User can change the number of events displayed.', ({ given, when, then }) => {
-        given('user would like to view a smaller or larger number of events', () => {
+        given(/^user sees (\d+) events and would like to view another number of events$/, (arg0) => {
 
         });
 
